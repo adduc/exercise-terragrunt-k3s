4621aa93d6cc6cf3d9e6367213e8ea44a9b31de4
John Long <john@128.io>
2024-07-17 22:47:07 -0500

    Wait until cluster is ready before marking container as created
    This allows fresh runs of `terragrunt run-all apply` to complete successfully

--- modules/docker_containers/docker_containers.tf
+++ modules/docker_containers/docker_containers.tf
@@ -29,13 +29,17 @@ resource "docker_image" "k3s" {
 }
 
 resource "docker_container" "k3s" {
-  name        = "${var.env_name}_k3s"
-  image       = docker_image.k3s.image_id
-  restart     = "unless-stopped"
-  stop_signal = "SIGKILL"
-  privileged  = true
-
+  name         = "${var.env_name}_k3s"
+  image        = docker_image.k3s.image_id
+  restart      = "unless-stopped"
+  stop_signal  = "SIGKILL"
+  privileged   = true
   network_mode = "bridge"
+
+  # wait for the cluster to be ready before considering the resource as
+  # created to allow subsequent terragrunt modules to interact with the
+  # cluster immediately
+  wait = true
 
   command = [
     "server",
@@ -74,6 +78,13 @@ resource "docker_container" "k3s" {
     name = "nproc"
     soft = 65535
     hard = 65535
+  }
+
+  healthcheck {
+    test     = ["CMD-SHELL", "kubectl get nodes"]
+    interval = "1s"
+    timeout  = "1s"
+    retries  = 15
   }
 }
 
